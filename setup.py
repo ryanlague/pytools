57a9fcd0ead31736f3a20453105a639ed7491d9a
ryanlague <ryanlague@hotmail.com>
2022-06-21 10:13:07 -0400

    Changed name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='xd_pytools',
+    name='pytools',
     version='1.0.0',
     description='A collections of helpful decorators for general use',
     author='Ryan Lague',
-    author_email='ryan@xdmind.com',
+    author_email='ryanlague@hotmail.com',
     packages=find_packages(),
     install_requires=[
         'pandas'
